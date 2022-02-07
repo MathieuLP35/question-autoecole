3b305c6232e5955d59fc33366d4df634bfb241e7
MathieuLP35 <36346321+MathieuLP35@users.noreply.github.com>
2022-02-07 15:03:48 +0100

    add views

--- App/routes/frontend.php
+++ App/routes/frontend.php
@@ -1,14 +1,11 @@
 <?php
 
-use Models\Client;
-
-$r->get('/', function () {
+$r->get('/admin', function () {
     Core\Template::prepare('pages.home')->render();
 });
 
-$r->post('/clients', function () {
-    $clients = Client::all();
-    var_dump($clients);
+$r->get('/', function () {
+    Core\Template::prepare('pages.admin')->render();
 });
 
-$r->get('/clients/{cid}', 'HomeController@totoRoute');
+// $r->get('/clients/{cid}', 'HomeController@totoRoute');
