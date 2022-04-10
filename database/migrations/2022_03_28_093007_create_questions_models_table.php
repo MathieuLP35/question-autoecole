207adb394062a33004b07350a4c3a1f1dc1dd96e
MathieuLP35 <36346321+MathieuLP35@users.noreply.github.com>
2022-04-10 16:19:10 +0200

    Modif migration

--- database/migrations/2022_03_28_093007_create_questions_models_table.php
+++ database/migrations/2022_03_28_093007_create_questions_models_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuestionModelsTable extends Migration
+class CreateQuestionsModelsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,7 +17,7 @@ class CreateQuestionModelsTable extends Migration
             $table->id();
             $table->string('texte')->charset("utf8")->collation("utf8_general_ci");
             $table->string('image')->charset("utf8")->collation("utf8_general_ci");
-            $table->json('propositions')();
+            $table->json('propositions');
             $table->timestamps();
         });
     }
