83e8095481e1eb98081891b6aae904f758f5592c
Ldelattre <leandre.delattre41@gmail.com>
2022-04-25 08:59:22 +0200

    add css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,34 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+<x-app-layout class="all">
+        <h2 class="bandeau">
+            titre
         </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+
+        <section class="main">
+            <h2>PRESENTATION DU SITE</h2>
+            <div class="corps">
+                <article class="article-1">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eligendi iste dolorem iusto nihil alias autem pariatur deserunt facere laboriosam cupiditate voluptatum ipsa velit tempora dolor in, asperiores soluta molestiae.
+                </article>
+
+                <article class="article-2">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio adipisci quo ratione minima deleniti exercitationem ipsa alias doloribus dolores similique nihil, nobis cum ipsum illum voluptatem odit accusantium harum necessitatibus?
+                </article>
+
+                <article class="article-3">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, provident quidem cumque esse nobis debitis voluptate ratione. Assumenda quisquam, nihil voluptatem, quos labore sit molestiae tenetur iure, atque quas reprehenderit!
+                </article>
+
+                <article class="article-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium expedita similique nam suscipit illum facilis dolores nulla perferendis neque ipsa vero unde nesciunt deleniti debitis, consequatur aliquid eveniet cumque reiciendis.
+                </article>
+
+                <article class="article-5">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, id ipsam necessitatibus quam fugiat ad. Eius vel dolores, accusamus explicabo quisquam blanditiis, debitis optio tenetur quod facere corrupti officia totam?
+                    <a href="#">INSCRIVEZ-VOUS</a>
+            </article>
+                
+                <!-- <x-jet-welcome /> -->
             </div>
-        </div>
-    </div>
+</section>
 </x-app-layout>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-100 nav-top">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,14 +6,21 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                        <!-- METTRE LE LOGO ICI -->
+                        <!-- <x-jet-application-mark class="block h-9 w-auto" /> -->
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('ACCUEIL') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('PRESENTATION') }}
+                    </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('PASSEZ LE TEST') }}
                     </x-jet-nav-link>
                     @if(Auth::user()->role === 3)
                         <x-jet-nav-link href="{{ route('admin') }}" :active="request()->routeIs('admin')">
