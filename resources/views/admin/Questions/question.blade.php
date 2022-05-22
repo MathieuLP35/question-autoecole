cd9c9580cabfc7a5d2c013f3f95d9a516e30e8ac
Fouad543 <77285308+Fouad543@users.noreply.github.com>
2022-05-22 19:25:15 +0200

    Front update

--- resources/views/admin/Questions/question.blade.php
+++ resources/views/admin/Questions/question.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 text-center leading-tight">
-            ❓&nbsp;{{ __('Question') }}&nbsp;❓
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            ❓&nbsp;{{ __('Administration') }}&nbsp;❓
         </h2>
     </x-slot>
 	@if (isset($question))
--- resources/views/admin/Questions/question_list.blade.php
+++ resources/views/admin/Questions/question_list.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Administration') }}
+        <a title="Acceuil Back-end"class="backend hover:underline"href="{{ route('admin') }}" :active="request()->routeIs('admin')">{{ __('Administration') }}</a>&nbsp;/&nbsp;Questions&nbsp;❓
         </h2>
     </x-slot>
     <div class="p-6 relative overflow-x-auto shadow-md sm:rounded-lg">
