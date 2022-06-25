3bb5b9ce7531f3fdd1bbaf7c956bae845eb0a0a4
MathieuLP <36346321+MathieuLP35@users.noreply.github.com>
2022-06-25 18:43:06 +0200

    Update 2022_03_28_090718_create_questionnaire_models_table.php

--- database/migrations/2022_03_28_090718_create_questionnaire_models_table.php
+++ database/migrations/2022_03_28_090718_create_questionnaire_models_table.php
@@ -20,7 +20,7 @@ class CreateQuestionnaireModelsTable extends Migration
             $table->unsignedBigInteger('groupe_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('groupe_id')->references('id')->on('groupes');
-            $table->integer('result');
+            $table->integer('result')->default(0);
             $table->timestamps();
         });
     }
