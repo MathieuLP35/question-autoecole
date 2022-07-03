a984025f76446a2ded0aba69d044fa2d749ede6b
leandre.delattre <leandre.delattre41@gmail.com>
2022-07-03 18:54:58 +0200

    page presentation ctn + style

--- resources/views/presentation.blade.php
+++ resources/views/presentation.blade.php
@@ -1,49 +1,89 @@
 <x-app-layout class="all">
 
-            <h1>BREIZH AUTO SKOL : Nos formule</h1>
+    <h1 class="presentation-title">BREIZH AUTO SKOL : Nos formule</h1>
 
 
-        <section class="main container">
-            <h2>PRESENTATION DU CONCEPT</h2>
-            <div class="corps">
-                <article class="article-1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eligendi iste dolorem iusto nihil alias autem pariatur deserunt facere laboriosam cupiditate voluptatum ipsa velit tempora dolor in, asperiores soluta molestiae.
-                </article>
+    <section class="main container">
+        <h2>Une auto ecole en ligne ? mais comment ca fonctionne ? </h2>
+        <div class="corps">
 
-            <article class="article-2">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio adipisci quo ratione minima deleniti exercitationem ipsa alias doloribus dolores similique nihil, nobis cum ipsum illum voluptatem odit accusantium harum necessitatibus?
+            <article class="presentation">
+                Une auto-école en ligne est une forme spécifique d’auto-école dont la mission reste la même que celle
+                des auto-écoles traditionnelles : accompagner des candidats dans leur <span
+                    class="highlight bold">préparation aux examens du code de
+                    la route et du permis de conduire</span>.
             </article>
 
-            <article class="article-3">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, provident quidem cumque esse nobis debitis voluptate ratione. Assumenda quisquam, nihil voluptatem, quos labore sit molestiae tenetur iure, atque quas reprehenderit!
+            <article class="presentation">
+                La principale différence entre les auto-écoles en ligne comme Breizh Auto Skol et les auto-écoles
+                classiques
+                tient dans le fait qu’une grande partie de la formation proposée par les auto-écoles en ligne est
+                <span class="highlight bold">dématérialisée</span>.
             </article>
 
-            <article class="article-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium expedita similique nam suscipit illum facilis dolores nulla perferendis neque ipsa vero unde nesciunt deleniti debitis, consequatur aliquid eveniet cumque reiciendis.
+            <article class="presentation">
+                Grâce à cela, vous pouvez réaliser la totalité de votre apprentissage à l’examen du Code de la route
+                directement en ligne. Vous pourrez ensuite découvrir la formation au permis de conduire dispensée par
+                des enseignants de la conduite diplômés d’État et partenaires de <span class="highlight bold">Breizh
+                    Auto Skol</span>, qui possèdent leur propre
+                véhicule double commande.
             </article>
 
-            <article class="article-5">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, id ipsam necessitatibus quam fugiat ad. Eius vel dolores, accusamus explicabo quisquam blanditiis, debitis optio tenetur quod facere corrupti officia totam?
-                <a href="#">INSCRIVEZ-VOUS</a>
+            <article class="presentation">
+                Enfin, même si notre auto-école est en ligne, notre équipe reste <span class="highlight bold">disponible
+                    à chaque instant afin de
+                    répondre à vos questions</span> ou à vos inquiétudes, mais également pour vous accompagner dans
+                toutes vos
+                démarches administratives, ceci depuis la réalisation de votre premier cours de code de la route jusqu’à
+                votre réussite à l’épreuve pratique. Alors qu'attendez vous ?
+                <a href="#">Demandez votre inscription</a>
             </article>
             <!-- <x-jet-welcome /> -->
         </div>
     </section>
 
-    <section>
-        
-        <h2>Nos Permis :</h2>
+    <section class="main container paragraph--cart">
 
-                <article class="article-1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eligendi iste dolorem iusto nihil alias autem pariatur deserunt facere laboriosam cupiditate voluptatum ipsa velit tempora dolor in, asperiores soluta molestiae.
-                </article>
+        <h2>Nos Permis</h2>
 
-                <article class="article-1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eligendi iste dolorem iusto nihil alias autem pariatur deserunt facere laboriosam cupiditate voluptatum ipsa velit tempora dolor in, asperiores soluta molestiae.
-                </article>
+        <article class="cart">
+            <img src="https://picsum.photos/250/250" alt="">
+            <div>
+                <h2>Permis B</h2>
+                <p>
+                    Le permis B permet de conduire des voitures dont le poids en charge est inférieur à 3,5 tonnes, et
+                    qui ne peuvent transporter plus de 8 passagers. Son obtention est soumise à des conditions d’âge, de
+                    formation et de réussite à un examen composé du code et d’une épreuve de conduite.
+                </p>
+                <a class="btn-blue" href="#">Nous contacter pour s'inscrire</a>
+            </div>
+        </article>
 
-                <article class="article-1">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab eligendi iste dolorem iusto nihil alias autem pariatur deserunt facere laboriosam cupiditate voluptatum ipsa velit tempora dolor in, asperiores soluta molestiae.
-                </article>
+        <article class="cart">
+            <img src="https://picsum.photos/250/250" alt="">
+            <div>
+                <h2>Permis A</h2>
+                <p>
+                    La catégorie A du permis de conduire permet la conduite de toutes les motocyclettes et tricycles
+                    ainsi que les quadricycles à moteur d’une puissance maximum de 15 kW.
+
+                    Un titulaire d’un permis A2 depuis plus de 2 ans peut se voir délivrer, après une formation de 7h,
+                    un permis de la catégorie A.
+                </p>
+                <a class="btn-blue" href="#">Nous contacter pour s'inscrire</a>
+            </div>
+        </article>
+
+        <article class="cart">
+            <img src="https://picsum.photos/250/250" alt="">
+            <div>
+                <h2>Permis C</h2>
+                <p>
+                    Le permis C1 permet de conduire des véhicules dont le PTAC est compris entre 3,5 et 7,5 tonnes et
+                    auxquels peut être attelée une remorque de moins de 750 kg.
+                </p>
+                <a class="btn-blue" href="#">Nous contacter pour s'inscrire</a>
+            </div>
+        </article>
     </section>
 </x-app-layout>
