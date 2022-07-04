60fc208d583182a92340dc91be81a8ab5f76bf29
MathieuLP <36346321+MathieuLP35@users.noreply.github.com>
2022-07-04 09:45:27 +0200

    fix question

--- resources/views/pages/test/test.blade.php
+++ resources/views/pages/test/test.blade.php
@@ -4,32 +4,31 @@
 			<a title="Acceuil" class="hover:underline" href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">{{ __('Accueil') }}</a>&nbsp;/&nbsp;Questions&nbsp;❓
         </h2>
     </x-slot>
-
-    {{-- On vérifie que $question n'est pas null --}}
-    @if(!empty($question))
-        <!-- <div class="flex justify-around flex-wrap p-8 gap-y-8"> -->
-            <div class="question">
-            <div class="question_illustration">
-                <img src="/storage/{{$question->image}}" alt="">
-            </div>
-            <form action="{{url('results/'.$question->id)}}" method="POST" class="w-full flex justify-around flex-wrap p-8 gap-y-8">
-                @csrf	
-                <div class="block w-full text-stone-50 p-4 bg-gray-700 rounded-lg">
-                    {{$question->texte}}
-                </div>
-                @foreach ($question->propositions as $proposition)
-                    <label for="proposition_{{$proposition['rep_id']}}">{{ $proposition['name'] }}</label>
-                    <input type="checkbox" id="proposition_{{$proposition['rep_id']}}" name="proposition_{{$proposition['rep_id']}}">
-                @endforeach
-                <div class="w-full flex justify-center">
-                    <input type="submit" class="btn-green m-2" value="Envoyer mes réponses">
-                </div>
-            </form>
-        </div>
-    @else
+    {{-- On vérifie que $question est null --}}
+    @if($question == "[]")
         <div class="block w-full text-center p-4 text-xl">
             Aucune question n'a été trouvée.
         </div>
+    @else
+        <!-- <div class="flex justify-around flex-wrap p-8 gap-y-8"> -->
+            <div class="question">
+                <div class="question_illustration">
+                    <img src="/storage/{{$question->image}}" alt="">
+                </div>
+                <form action="{{url('results/'.$question->id)}}" method="POST" class="w-full flex justify-around flex-wrap p-8 gap-y-8">
+                    @csrf	
+                    <div class="block w-full text-stone-50 p-4 bg-gray-700 rounded-lg">
+                        {{$question->texte}}
+                    </div>
+                    @foreach ($question->propositions as $proposition)
+                        <label for="proposition_{{$proposition['rep_id']}}">{{ $proposition['name'] }}</label>
+                        <input type="checkbox" id="proposition_{{$proposition['rep_id']}}" name="proposition_{{$proposition['rep_id']}}">
+                    @endforeach
+                    <div class="w-full flex justify-center">
+                        <input type="submit" class="btn-green m-2" value="Envoyer mes réponses">
+                    </div>
+                </form>
+            </div>
     @endif
 
 </x-app-layout>
