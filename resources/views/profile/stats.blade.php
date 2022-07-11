b30d2d853e14583f0279aca22bde64deb5c15b9d
Ldelattre <leandre.delattre41@gmail.com>
2022-07-11 14:31:40 +0200

    modifications styles stats

--- resources/views/profile/stats.blade.php
+++ resources/views/profile/stats.blade.php
@@ -1,24 +1,39 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="breadcrump">
             {{ __('Statistiques') }}
         </h2>
     </x-slot>
     @if (isset($user))
-	<div class="flex justify-center flex-wrap bg-black p-8 gap-y-8">
-		<div class="block text-center w-full mx-1.5 text-stone-50 p-4 bg-gray-700 rounded-lg">
-            {{$user->name}}
+	<div class="container stat-corps">
+		<div class="stat-label">
+            <p>
+                Profil :
+            </p>
+            <p>
+                {{$user->name}}
+            </p>
         </div>
-		<div class="block text-center w-full mx-1.5 text-stone-50 p-4 bg-gray-700 rounded-lg">
-            {{$moyenne}} % de reussite
+		<div class="stat-label">
+            <p>
+                Votre moyenne : 
+            </p>
+            <p>
+                {{$moyenne}} % de reussite
+            </p>
         </div>
-		<div class="block text-center w-full mx-1.5 text-stone-50 p-4 bg-gray-600 rounded-lg">
+		<div class="stat-label">
             Historique des Résultats
         </div>
-        <ul class="flex justify-center flex-wrap gap-y-3">
+        <ul class="stat-list">
             @foreach ($scores as $score)
-            <li class="block text-center w-full mx-1.5 text-stone-50 p-4 bg-gray-500 rounded-lg">
-                {{$score->moy}} % le {{date('d/m/Y', strtotime($score->created_at))}}
+            <li class="stat-item">
+                <p>
+                    {{$score->moy}} % 
+                </p>
+                <p>
+                    {{date('d/m/Y', strtotime($score->created_at))}}
+                </p>
             </li>
             @endforeach
         </ul>
