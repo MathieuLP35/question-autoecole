19fc6a0fceb3a51f0f4eaa7fc29ab0ad853c6766
Fouad543 <fouadhimdi@hotmail.fr>
2022-07-11 10:32:31 +0200

    Revert "Stats utilisateurs dans onglets Statistiques"
    This reverts commit 560e0420df664e8a098289e4e27d1fb815452726.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,15 +4,6 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-Alpine.data('demo', () => ({
-   open: false,
-
-   toggle() {
-       this.open = !this.open
-   }
-}));
-
 window.Alpine = Alpine;
 
-// should be last
 Alpine.start();
