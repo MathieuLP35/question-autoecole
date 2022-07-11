ca8f93ebc71fa6b89f859ff19b79e35b50625ad7
MathieuLP <36346321+MathieuLP35@users.noreply.github.com>
2022-07-11 15:06:36 +0200

    remove teams

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\UtilisateurController;
 use App\Http\COntrollers\GroupeController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\ScoreController;
-use Faker\Core\File;
 use Illuminate\Support\Facades\Route;
 
 /*
