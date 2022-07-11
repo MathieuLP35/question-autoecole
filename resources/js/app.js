560e0420df664e8a098289e4e27d1fb815452726
Fouad543 <fouadhimdi@hotmail.fr>
2022-07-11 10:31:15 +0200

    Stats utilisateurs dans onglets Statistiques

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,15 @@ import './validation';
 
 import Alpine from 'alpinejs';
 
+Alpine.data('demo', () => ({
+   open: false,
+
+   toggle() {
+       this.open = !this.open
+   }
+}));
+
 window.Alpine = Alpine;
 
+// should be last
 Alpine.start();
